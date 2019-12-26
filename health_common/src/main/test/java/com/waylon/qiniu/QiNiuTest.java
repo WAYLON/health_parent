14352a88848fa2cf15e1f48d6d174e88d3776638
wanglei-007 <804818167@qq.com>
2019-12-26 22:24:54 +0800

    七牛云-文件上传测试

---
+++ health_common/src/main/test/java/com/waylon/qiniu/QiNiuTest.java
@@ -0,0 +1,47 @@
+package com.waylon.qiniu;
+
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.Region;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
+import org.junit.Test;
+
+public class QiNiuTest {
+
+    @Test
+    public void test1() {
+        //构造一个带指定 Region 对象的配置类
+        Configuration cfg = new Configuration(Region.region2());
+        //...其他参数参考类注释
+        UploadManager uploadManager = new UploadManager(cfg);
+        //...生成上传凭证，然后准备上传
+        String accessKey = "C6WjFfvJ44iSIn5JvEUf8MZvXE7UsDYAvUAqhXlQ";
+        String secretKey = "Nqiq65P0gni1RktgvE9XY73LJL4lM38ftvit6AnM";
+        String bucket = "waylon-hea lth";
+        //如果是Windows情况下，格式是 D:\\qiniu\\test.png
+        String localFilePath = "C:\\Users\\80481\\Desktop\\1.jpg";
+        //默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = null;
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
+        try {
+            Response response = uploadManager.put(localFilePath, key, upToken);
+            //解析上传成功的结果
+            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+            System.out.println(putRet.key);
+            System.out.println(putRet.hash);
+        } catch (QiniuException ex) {
+            Response r = ex.response;
+            System.err.println(r.toString());
+            try {
+                System.err.println(r.bodyString());
+            } catch (QiniuException ex2) {
+                //ignore
+            }
+        }
+    }
+}
